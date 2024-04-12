8cf94506cbd4015fe49ff4f58b0eb38a02ef3efc
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-04-12 14:36:31 -0300

    :wrench: chore: add env config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EnvConfigModule } from './shared/infraestructure/env-config/env-config.module';
 
 @Module({
-  imports: [],
+  imports: [EnvConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
