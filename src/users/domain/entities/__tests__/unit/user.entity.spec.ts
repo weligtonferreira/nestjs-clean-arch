44582a611115eed705d0a7ff292689f98ef8c2d8
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-04-22 18:09:43 -0300

    :test_tube: include unit tests for validate method in UserEntity

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -6,12 +6,13 @@ describe('UserEntity unit tests', () => {
   let sut: UserEntity;
 
   beforeEach(() => {
+    UserEntity.validate = jest.fn();
     props = UserDataBuilder({});
-
     sut = new UserEntity(props);
   });
 
   it('Constructor method must create the object correctly', () => {
+    expect(UserEntity.validate).toHaveBeenCalled();
     expect(sut.props.name).toEqual(props.name);
     expect(sut.props.email).toEqual(props.email);
     expect(sut.props.password).toEqual(props.password);
@@ -55,11 +56,13 @@ describe('UserEntity unit tests', () => {
 
   it('Should update a user', () => {
     sut.update('other name');
+    expect(UserEntity.validate).toHaveBeenCalled();
     expect(sut.props.name).toEqual('other name');
   });
 
   it('Should update the password field', () => {
     sut.updatePassword('other password');
+    expect(UserEntity.validate).toHaveBeenCalled();
     expect(sut.props.password).toEqual('other password');
   });
 });
