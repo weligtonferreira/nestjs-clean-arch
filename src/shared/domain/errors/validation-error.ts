052aa361f33199973d96ed875d27a6524d0b8e59
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-04-26 15:34:12 -0300

    :recycle: refactor: rename error message

--- src/shared/domain/errors/validation-error.ts
+++ src/shared/domain/errors/validation-error.ts
@@ -4,7 +4,7 @@ export class ValidationError extends Error {}
 
 export class EntityValidationError extends Error {
   constructor(public error: FieldsError) {
-    super('Entity Validation Eroor');
+    super('Entity Validation Error');
     this.name = 'EntityValidationError';
   }
 }
