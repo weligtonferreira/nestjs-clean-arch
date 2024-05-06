caeb4f2115808d2de79912050a19d12330a918d3
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-05-06 18:34:25 -0300

    :test_tube: test: add findById method unit tests for InMemoryRepository

--- src/shared/domain/repository/__tests__/unit/in-memory-repository.spec.ts
+++ src/shared/domain/repository/__tests__/unit/in-memory-repository.spec.ts
@@ -1,5 +1,6 @@
 import { Entity } from '@/shared/domain/entities/entity';
 import { InMemoryRepository } from '../../in-memory-repository';
+import { NotFoundError } from '@/shared/domain/errors/not-found-error';
 
 type StubEntityProps = {
   name: string;
@@ -22,5 +23,19 @@ describe('InMemoryRepository unit tests', () => {
     await sut.insert(entity);
 
     expect(entity.toJSON()).toStrictEqual(sut.items[0].toJSON());
+  });
+
+  it('Should throw an error when entity is not found', async () => {
+    await expect(sut.findById('fakeId')).rejects.toThrow(
+      new NotFoundError('Entity not found'),
+    );
+  });
+
+  it('Should find an entity by id', async () => {
+    const entity = new StubEntity({ name: 'test name', price: 50 });
+    await sut.insert(entity);
+    const result = await sut.findById(entity._id);
+
+    expect(entity.toJSON()).toStrictEqual(result.toJSON());
   });
 });
