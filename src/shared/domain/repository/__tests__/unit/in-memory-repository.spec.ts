73d9974d0485414a8eff5ca1e73ddccc71743bc7
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-05-13 11:20:07 -0300

    :test_tube: test: add update method unit tests for InMemoryRepository

--- src/shared/domain/repository/__tests__/unit/in-memory-repository.spec.ts
+++ src/shared/domain/repository/__tests__/unit/in-memory-repository.spec.ts
@@ -46,4 +46,25 @@ describe('InMemoryRepository unit tests', () => {
 
     expect([entity]).toStrictEqual(result);
   });
+
+  it('Should throw an error on update when entity is not found', async () => {
+    const entity = new StubEntity({ name: 'test name', price: 50 });
+
+    await expect(sut.update(entity)).rejects.toThrow(
+      new NotFoundError('Entity not found'),
+    );
+  });
+
+  it('Should update an entity by id', async () => {
+    const entity = new StubEntity({ name: 'test name', price: 50 });
+    await sut.insert(entity);
+    const entityUpdated = new StubEntity(
+      { name: 'updated', price: 10 },
+      entity._id,
+    );
+
+    await sut.update(entityUpdated);
+
+    expect(entityUpdated.toJSON()).toStrictEqual(sut.items[0].toJSON());
+  });
 });
