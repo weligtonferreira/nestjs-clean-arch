ddc5105ab5a955a8b9b81658dabfbf35a21c5f71
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-05-23 17:21:07 -0300

    :sparkles: feat: add SearchProps type

--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -1,6 +1,16 @@
 import { Entity } from '../entities/entity';
 import { RepositoryInterface } from './repository-contracts';
 
+export type SortDirection = 'asc' | 'desc';
+
+export type SearchProps<Filter = string> = {
+  page?: number;
+  perPage?: number;
+  sort?: string | null;
+  sortDir?: SortDirection | null;
+  filter?: Filter | null;
+};
+
 export interface SearchableRepositoryInterface<
   E extends Entity,
   SearchInput,
