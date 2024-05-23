fb6a50dec6e9cbf34b9739c85dcf25df81122ab2
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-05-23 16:13:32 -0300

    :recycle: refactor: change directory name

--- src/users/domain/repositories/user.repository.ts
+++ src/users/domain/repositories/user.repository.ts
@@ -1,4 +1,4 @@
-import { RepositoryInterface } from '@/shared/domain/repository/repository-contracts';
+import { RepositoryInterface } from '@/shared/domain/repositories/repository-contracts';
 import { UserEntity } from '../entities/user.entity';
 
 export interface UserRepository extends RepositoryInterface<UserEntity> {
--- src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
+++ src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
@@ -1,7 +1,7 @@
 import { UserEntity } from '@/users/domain/entities/user.entity';
 
 import { UserRepository } from '@/users/domain/repositories/user.repository';
-import { InMemoryRepository } from '@/shared/domain/repository/in-memory-repository';
+import { InMemoryRepository } from '@/shared/domain/repositories/in-memory-repository';
 
 import { ConflictError } from '@/shared/domain/errors/conflict-error';
 import { NotFoundError } from '@/shared/domain/errors/not-found-error';
