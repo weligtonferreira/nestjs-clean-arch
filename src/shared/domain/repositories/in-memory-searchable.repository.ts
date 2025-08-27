0bf343ef9f9b4e39753529708f28588fa3163ea7
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2025-08-27 15:22:11 -0300

    :sparkles: feat: implement functions in InMemorySearchableRepository class

---
+++ src/shared/domain/repositories/in-memory-searchable.repository.ts
@@ -0,0 +1,79 @@
+import { Entity } from '../entities/entity';
+import { InMemoryRepository } from './in-memory.repository';
+import {
+  SearchableRepositoryInterface,
+  SearchParams,
+  SearchResult,
+} from './searchable-repository-contracts';
+
+export abstract class InMemorySearchableRepository<E extends Entity>
+  extends InMemoryRepository<E>
+  implements SearchableRepositoryInterface<E, any, any>
+{
+  sortableFields: string[] = [];
+
+  async search(props: SearchParams): Promise<SearchResult<E>> {
+    const itemsFiltered = await this.applyFilter(this.items, props.filter);
+
+    const itemsSorted = await this.applySort(
+      itemsFiltered,
+      props.sort,
+      props.sortDir,
+    );
+
+    const itemsPaginated = await this.applyPaginate(
+      itemsSorted,
+      props.page,
+      props.perPage,
+    );
+
+    return new SearchResult({
+      items: itemsPaginated,
+      total: itemsFiltered.length,
+      currentPage: props.page,
+      perPage: props.perPage,
+      sort: props.sort,
+      sortDir: props.sortDir,
+      filter: props.filter,
+    });
+  }
+
+  protected abstract applyFilter(
+    items: E[],
+    filter: string | null,
+  ): Promise<E[]>;
+
+  protected async applySort(
+    items: E[],
+    sort: string | null,
+    sortDir: string | null,
+  ): Promise<E[]> {
+    if (sort === null || this.sortableFields.includes(sort) === false) {
+      return items;
+    }
+
+    const newSortedItems = [...items].sort((a, b) => {
+      if (a.props[sort] < b.props[sort]) {
+        return sortDir === 'asc' ? -1 : 1;
+      }
+
+      if (a.props[sort] > b.props[sort]) {
+        return sortDir === 'asc' ? 1 : -1;
+      }
+
+      return 0;
+    });
+
+    return newSortedItems;
+  }
+
+  protected async applyPaginate(
+    items: E[],
+    page: SearchParams['page'],
+    perPage: SearchParams['perPage'],
+  ): Promise<E[]> {
+    const start = (page - 1) * perPage;
+    const limit = start + perPage;
+    return items.slice(start, limit);
+  }
+}
