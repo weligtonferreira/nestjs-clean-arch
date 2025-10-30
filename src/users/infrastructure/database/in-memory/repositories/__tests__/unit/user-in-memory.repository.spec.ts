6f13b0b66bcc4dc2eafdfa4fb110b234da4be413
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2025-10-30 16:40:03 -0300

    :test_tube: test(UserInMemoryRepository): create unit tests

---
+++ src/users/infrastructure/database/in-memory/repositories/__tests__/unit/user-in-memory.repository.spec.ts
@@ -0,0 +1,98 @@
+import { ConflictError } from '@/shared/domain/errors/conflict-error';
+import { NotFoundError } from '@/shared/domain/errors/not-found-error';
+import { UserEntity } from '@/users/domain/entities/user.entity';
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
+import { UserInMemoryRepository } from '../../user-in-memory.repository';
+
+describe('UserInMemoryRepository unit tests', () => {
+  let sut: UserInMemoryRepository;
+
+  beforeAll(() => {
+    sut = new UserInMemoryRepository();
+  });
+
+  it('Should throw error when not found - findByEmail method', async () => {
+    await expect(sut.findByEmail('a@a.com')).rejects.toThrow(
+      new NotFoundError('Entity not found using email: a@a.com'),
+    );
+  });
+
+  it('Should find a entity by email - findByEmail method', async () => {
+    const entity = new UserEntity(UserDataBuilder({}));
+    await sut.insert(entity);
+    const result = await sut.findByEmail(entity.email);
+
+    expect(entity.toJSON()).toStrictEqual(result.toJSON());
+  });
+
+  it('Should generate an error when there is already a user with the same email - emailExists method', async () => {
+    const entity = new UserEntity(UserDataBuilder({}));
+    await sut.insert(entity);
+
+    await expect(sut.emailExists(entity.email)).rejects.toThrow(
+      new ConflictError('Email address already used'),
+    );
+  });
+
+  it('Should no filter items when filter object is null', async () => {
+    const entity = new UserEntity(UserDataBuilder({}));
+    await sut.insert(entity);
+    const result = await sut.findAll();
+    const spyFilter = jest.spyOn(result, 'filter');
+    const itemsFiltered = await sut['applyFilter'](result, null);
+
+    expect(spyFilter).not.toHaveBeenCalled();
+    expect(itemsFiltered).toStrictEqual(result);
+  });
+
+  it('Should filter name field using filter param', async () => {
+    const items = [
+      new UserEntity(UserDataBuilder({ name: 'Test' })),
+      new UserEntity(UserDataBuilder({ name: 'TEST' })),
+      new UserEntity(UserDataBuilder({ name: 'fake' })),
+    ];
+    const spyFilter = jest.spyOn(items, 'filter');
+    const itemsFiltered = await sut['applyFilter'](items, 'TEST');
+
+    expect(spyFilter).toHaveBeenCalled();
+    expect(itemsFiltered).toStrictEqual([items[0], items[1]]);
+  });
+
+  it('Should sort by createdAt when sort param is null', async () => {
+    const createdAt = new Date();
+
+    const items = [
+      new UserEntity(UserDataBuilder({ name: 'Test', createdAt })),
+      new UserEntity(
+        UserDataBuilder({
+          name: 'TEST',
+          createdAt: new Date(createdAt.getTime() + 1),
+        }),
+      ),
+      new UserEntity(
+        UserDataBuilder({
+          name: 'fake',
+          createdAt: new Date(createdAt.getTime() + 2),
+        }),
+      ),
+    ];
+
+    const itemsSorted = await sut['applySort'](items, null, null);
+
+    expect(itemsSorted).toStrictEqual([items[2], items[1], items[0]]);
+  });
+
+  it('Should sort by name field', async () => {
+    const items = [
+      new UserEntity(UserDataBuilder({ name: 'b' })),
+      new UserEntity(UserDataBuilder({ name: 'c' })),
+      new UserEntity(UserDataBuilder({ name: 'a' })),
+    ];
+
+    let itemsSorted = await sut['applySort'](items, 'name', 'asc');
+    expect(itemsSorted).toStrictEqual([items[2], items[0], items[1]]);
+
+    itemsSorted = await sut['applySort'](items, 'name', null);
+    expect(itemsSorted).toStrictEqual([items[1], items[0], items[2]]);
+  });
+});
